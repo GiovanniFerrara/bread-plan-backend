bca2e5bd967f3026e2ac299b4ff595829563ce07
Giovanni Ferrara <giovanni.ferrara@ergo.digital>
2021-03-24 19:42:25 +0100

    wipe db before testing

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,5 @@
 NODE_ENV="test"
+rm -rf ./db-volume
 npx sequelize db:migrate:undo:all --env test
 npx sequelize-cli db:migrate --url 'postgres://postgres:password@postgres-db/postgres' --env test
 npm run build
