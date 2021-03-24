41afc61dd67706104948b1bb7626ace0da7c65f1
Giovanni Ferrara <giovanni.ferrara@ergo.digital>
2021-03-24 19:32:04 +0100

    build first

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,5 @@
 NODE_ENV="test"
 npx sequelize db:migrate:undo:all --env test
 npx sequelize-cli db:migrate --url 'postgres://postgres:password@postgres-db/postgres' --env test
+npm run build
 jest --watchAll
